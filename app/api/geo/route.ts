b6dd5ddab6df875adb908e6dd59d886c19b5a6e4
adrianAlday <adrian.alday@gmail.com>
2026-05-25 17:38:55 -0400

    geolocation endpoint

--- app/api/geo/route.ts
+++ app/api/geo/route.ts
@@ -1,10 +1,19 @@
+import { isDev } from "@/app/_utils/isDev";
 import { geolocation } from "@vercel/functions";
 
 export const GET = (request: Request) => {
-  console.log(request);
-
-  const details = geolocation(request);
-  console.log(details);
+  const details = isDev
+    ? {
+        city: "Alexandria",
+        country: "US",
+        flag: "🇺🇸",
+        countryRegion: "VA",
+        region: "iad1",
+        latitude: "38.8423",
+        longitude: "-77.0593",
+        postalCode: "22305",
+      }
+    : geolocation(request);
 
   return Response.json(details);
 };
