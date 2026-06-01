14cc94856f58cd0d581aa37e79bd52b8c656aecc
adrianAlday <adrian.alday@gmail.com>
2026-06-01 12:17:22 -0400

    toast smaller icon, icon background, subtitle lines and font size

--- app/_components/ToastContext.tsx
+++ app/_components/ToastContext.tsx
@@ -76,23 +76,21 @@ export const ToastProvider = ({ children }: ToastProvideProps) => {
           <div className="border border-[#3d444d] rounded-md px-2 py-2 flex">
             <div
               className={
-                "mr-2 border border-[#3d444d] rounded-[22.5%] h-15 shrink-0 aspect-square relative"
+                "mr-2 border border-[#3d444d] rounded-[22.5%] h-13 shrink-0 aspect-square relative"
               }
             >
               <Image
                 src={"/pin.png"}
                 alt={"wya"}
                 fill
-                className={"rounded-[22.5%] p-2"}
+                className={"rounded-[22.5%] p-2 bg-[rgb(219,231,203)]"}
               />
             </div>
 
-            <div className="grow text-sm truncate">
+            <div className="grow text-md truncate">
               <div className="font-semibold">{toast.title}</div>
 
-              <div className="line-clamp-2 text-wrap break-all text-ellipsis">
-                {toast.subtitle}
-              </div>
+              <div className="truncate">{toast.subtitle}</div>
             </div>
           </div>
         </div>
