dc43b0f1e3e150151f514affb811471f2a0e6342
adrianAlday <adrian.alday@gmail.com>
2026-06-02 12:11:09 -0400

    delay animate in

--- app/_components/ToastContext.tsx
+++ app/_components/ToastContext.tsx
@@ -60,7 +60,7 @@ export const ToastProvider = ({ children }: ToastProvideProps) => {
           );
         }, transitionDuration);
       }, 1000);
-    }, 1);
+    }, 10);
   }, []);
 
   return (
@@ -71,7 +71,7 @@ export const ToastProvider = ({ children }: ToastProvideProps) => {
         <div
           key={toast.id}
           id={toast.id}
-          className={`absolute top-0 z-50 w-dvw ${pageIsFullWidth ? "" : pageMaxWidthClass} bg-[#151b23] pt-2 px-2 translate-y-[calc(-100%)] opacity-0 invisible transition-all ${uninterpolatedDurationClass} ease-out open:translate-y-0 open:opacity-100 open:visible`}
+          className={`absolute top-0 z-50 w-dvw ${pageIsFullWidth ? "" : pageMaxWidthClass} bg-[#151b23] pt-2 px-2 transition-all ${uninterpolatedDurationClass} ease-in-out translate-y-[calc(-100%)] opacity-0 invisible open:translate-y-0 open:opacity-100 open:visible`}
         >
           <div className="border border-[#3d444d] rounded-md px-2 py-2 flex">
             <div
