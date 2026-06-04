c0615d936300a976d329b3d4ad0637fa27c19020
adrianAlday <adrian.alday@gmail.com>
2026-06-04 15:46:37 -0400

    todos

--- app/page.tsx
+++ app/page.tsx
@@ -28,3 +28,36 @@ const HomePage = async () => (
 );
 
 export default HomePage;
+
+// to do:
+// add classnames library
+// handle multiple locations, use query string
+// rename repo to wya-maps?
+// search suggestions with numbered results and markers ?
+// more info for picker map
+// different languagers lets go
+// [
+//   "Let's go",
+//   "Vamos",
+//   "Allez",
+//   "Andiamo",
+//   "Auf geht's",
+//   "Ikimashou",
+//   "Gaja",
+//   "Zǒuba",
+// ];
+// real modal for title
+// use apple clear icon?
+// clear button for pin search
+// make place pin draggable when new?
+// add title to link out queries to like google?
+// add apple icon
+// allow toasts with diff icons?
+// fix title saved toast fires when sharing
+// edit title button?
+// apple maps auto complete?
+// go button still too low on pwa
+// pwa manifest
+// get apple symbols
+// change svgs on line meassure thing
+// reverse geocode and get hours from apple maps?
