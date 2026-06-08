ab2417796419100aa95887cffb24e2b50307705e
adrianAlday <adrian.alday@gmail.com>
2026-06-08 12:58:31 -0400

    loading wrapper without spinner

--- app/_components/Spinner.tsx
+++ app/_components/Spinner.tsx
@@ -3,9 +3,7 @@ type SpinnerProps = {
 };
 
 const Spinner = ({ classNames }: SpinnerProps) => (
-  <div
-    className={`flex items-center justify-center h-[calc(100vh-200px)] ${classNames}`}
-  >
+  <div className={`flex items-center justify-center h-dvh ${classNames}`}>
     <svg
       className="mr-3 -ml-1 size-5 animate-spin text-[rgb(189,190,191)]"
       fill="none"
