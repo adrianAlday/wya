b698cdc3da910704e7082ba61d4e1a1b3b8185be
adrianAlday <adrian.alday@gmail.com>
2026-06-08 11:37:03 -0400

    more footer margin

--- app/_components/FooterLink.tsx
+++ app/_components/FooterLink.tsx
@@ -10,7 +10,7 @@ const FooterLink = ({ url, text }: FooterLinkProps) => (
     target="_blank"
     href={url}
     className={
-      "mt-2.5 text-xs text-[rgb(74,74,74)] hover:text-[rgb(146,152,160)] active:text-[rgb(89,149,242)] active:underline"
+      "mt-4 text-xs text-[rgb(74,74,74)] hover:text-[rgb(146,152,160)] active:text-[rgb(89,149,242)] active:underline"
     }
   >
     {text}
