56768ecd3e6c5bc615b1f53f89321c6d7ac0dc11
adrianAlday <adrian.alday@gmail.com>
2026-06-09 13:39:00 -0400

    home bounce loader

--- app/_components/HomeMap.tsx
+++ app/_components/HomeMap.tsx
@@ -316,31 +316,14 @@ const HomeMap = ({ latitude, longitude }: HomeMapProps) => {
 
   return (
     <div className="w-dvw">
-      {/* <Spinner classNames={loading ? "block" : "hidden"} /> */}
-      <div
-        className={`flex items-center justify-center h-dvh ${loading ? "block" : "hidden"}`}
-      >
-        <svg
-          className="mr-3 -ml-1 size-5 animate-spin text-[rgb(0,255,0)]"
-          fill="none"
-          viewBox="0 0 24 24"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <circle
-            className="opacity-33"
-            cx="12"
-            cy="12"
-            r="10"
-            stroke="currentColor"
-            strokeWidth="4"
-          />
+      <div className={`mt-8 ml-13 ${loading ? "block" : "hidden"}`}>
+        <div className="flex space-x-2">
+          <div className="rounded-full h-2 w-2 bg-[rgb(189,190,191)] animate-bounce [animation-delay:-0.3s]" />
 
-          <path
-            className="opacity-100"
-            fill="currentColor"
-            d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-          />
-        </svg>
+          <div className="rounded-full h-2 w-2 bg-[rgb(189,190,191)] animate-bounce [animation-delay:-0.15s]" />
+
+          <div className="rounded-full h-2 w-2 bg-[rgb(189,190,191)] animate-bounce" />
+        </div>
       </div>
 
       <style>
