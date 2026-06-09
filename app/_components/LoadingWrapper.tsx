c12e938ff7e60303ead51ae7f85f279e2e86cbe1
adrianAlday <adrian.alday@gmail.com>
2026-06-09 12:38:44 -0400

    bounce loader

--- app/_components/LoadingWrapper.tsx
+++ app/_components/LoadingWrapper.tsx
@@ -1,5 +1,4 @@
 import { Suspense } from "react";
-// import Spinner from "./Spinner";
 
 type LoadingWrapperProps = {
   children: React.ReactNode;
@@ -9,53 +8,20 @@ const LoadingWrapper = ({ children }: LoadingWrapperProps) => (
   <Suspense
     fallback={
       <div>
-        {/* <Spinner /> */}
-        <div className={`flex items-center justify-center h-dvh`}>
-          <svg
-            className="mr-3 -ml-1 size-5 animate-spin text-[rgb(255,0,0)]"
-            fill="none"
-            viewBox="0 0 24 24"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <circle
-              className="opacity-33"
-              cx="12"
-              cy="12"
-              r="10"
-              stroke="currentColor"
-              strokeWidth="4"
-            />
+        <div className="mt-8 ml-13">
+          <div className="flex space-x-2">
+            <div className="rounded-full h-2 w-2 bg-[rgb(189,190,191)] animate-bounce [animation-delay:-0.3s]" />
 
-            <path
-              className="opacity-100"
-              fill="currentColor"
-              d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-            />
-          </svg>
+            <div className="rounded-full h-2 w-2 bg-[rgb(189,190,191)] animate-bounce [animation-delay:-0.15s]" />
+
+            <div className="rounded-full h-2 w-2 bg-[rgb(189,190,191)] animate-bounce" />
+          </div>
         </div>
 
         {
           // more data needed to display on mobile
         }
-        <div className="text-[rgb(22,27,34)] h-0">
-          There{"'"}s a wise saying that goes like this: A real gentleman never
-          discusses women he{"'"}s broken up with or how much tax he{"'"}s paid.
-          Actually, this is a total lie. I just made it up. Sorry! But if there
-          really were such a saying, I think that one more condition for being a
-          gentleman would be keeping quiet about what you do to stay healthy. A
-          gentleman shouldn{"'"}t go on and on about what he does to stay fit.
-          At least that{"'"}s how I see it.
-        </div>
-        <div className="text-[rgb(22,27,34)] h-0">
-          There{"'"}s a wise saying that goes like this: A real gentleman never
-          discusses women he{"'"}s broken up with or how much tax he{"'"}s paid.
-          Actually, this is a total lie. I just made it up. Sorry! But if there
-          really were such a saying, I think that one more condition for being a
-          gentleman would be keeping quiet about what you do to stay healthy. A
-          gentleman shouldn{"'"}t go on and on about what he does to stay fit.
-          At least that{"'"}s how I see it.
-        </div>
-        <div className="text-[rgb(22,27,34)] h-0">
+        <div className="text-[rgb(22,27,34)]">
           There{"'"}s a wise saying that goes like this: A real gentleman never
           discusses women he{"'"}s broken up with or how much tax he{"'"}s paid.
           Actually, this is a total lie. I just made it up. Sorry! But if there
