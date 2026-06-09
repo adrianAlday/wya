63002b27edd5fed6b33fa8644ccec41eacdfbbf7
adrianAlday <adrian.alday@gmail.com>
2026-06-09 14:41:56 -0400

    center bouncer

--- app/_components/Bouncer.tsx
+++ app/_components/Bouncer.tsx
@@ -3,7 +3,9 @@ type BouncerProps = {
 };
 
 const Bouncer = ({ classNames }: BouncerProps) => (
-  <div className={`mt-9 ml-15 animate-pulse ${classNames}`}>
+  <div
+    className={`mt-9 sm:mt-7 flex justify-center animate-pulse ${classNames}`}
+  >
     <div className="flex space-x-2">
       <div className="rounded-full h-2 w-2 bg-[rgb(189,190,191)] animate-bounce [animation-delay:-0.3s]" />
 
