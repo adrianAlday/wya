985377b8d47b7b6ec474fcf49dc70bfaf2a4a407
adrianAlday <adrian.alday@gmail.com>
2026-06-10 15:16:42 -0400

    log strava output on prod

--- app/api/geojson/route.ts
+++ app/api/geojson/route.ts
@@ -54,6 +54,8 @@ export const POST = async (request: NextRequest) => {
           stravaRequestOptions,
         ).then(async (response) => await response.json());
 
+        console.log(streams);
+
         const lngLats = streams?.latlng.map((latlng: number[]) =>
           latlng.reverse(),
         );
@@ -66,6 +68,8 @@ export const POST = async (request: NextRequest) => {
           `https://www.strava.com/routes/${value}/export_gpx`,
           stravaRequestOptions,
         ).then(async (response) => await response.text());
+
+        console.log(gpx);
 
         const lngLats = parseGpx(gpx);
 
